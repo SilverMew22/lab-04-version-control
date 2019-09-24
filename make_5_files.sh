2ae43246ac9121c9134ddae9489b4691acf6ae9f
SGarnett <Shequana.Garnett.mail.citytech.cuny.edu>
2019-09-24 14:05:18 -0400

    added a new .sh file

---
+++ make_5_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {1..10}
+do
+   echo "myfile$i.txt"
+done
---
+++ say_twice.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cat /Users/student/Desktop/SGarnett/lab-04-version-control
+echo ---
+cat /Users/student/Desktop/SGarnett/lab-04-version-control
+touch foo.txt
+echo "foo bar baz"
